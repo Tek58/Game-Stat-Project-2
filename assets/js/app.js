c7e80a37e61efa86dbf266442d5b46584ffee376
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2021-02-28 03:39:58 +0300

    details of index updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,16 @@
 let myHeaders = new Headers();
 myHeaders.append('Content-Type', 'image/jpeg');
 
-var heading = document.querySelector("#headings")
+var heading = document.querySelector("#headings");
 var headingWapons = document.querySelector('#headingsWeapons')
+var catagory = document.querySelector('#CatagoryRow')
 var kills = document.querySelector("#killsRow");
 var shotsFired = document.querySelector("#shotsFiredRow");
 var shotsHit = document.querySelector("#shotshitRow");
 var shotsAccuracy = document.querySelector("#shotsAccuracyRow");
 var profileDiv = document.querySelector(".profileDiv");
 var search = document.querySelector(".search_icon");
+var titles = document.querySelector(".avatarTitles")
 
 
 
@@ -44,6 +46,7 @@ function apiFetch(name){
     .then(function(data){
         let i ;
         heading.innerHTML = '<th scope="col">Weapons</th>'
+        catagory.innerHTML =   '<th scope="col">Catagory</th>'
         kills.innerHTML = '<th scope="row">KILLS</th>'
         shotsFired.innerHTML ='<th scope="row">SHOTS FIRED</th>'
         shotsHit.innerHTML = '<th scope="row">SHOTS HIT</th>'
@@ -53,6 +56,7 @@ function apiFetch(name){
         for(i=0;i<Object.keys(dataElems).length;i++){
             heading.innerHTML += `<th scope="col">${dataElems[i]["attributes"]["key"].toUpperCase()}</th>`
             headingWapons.innerHTML += `<th scope="col"><img src="${dataElems[i]["metadata"]["imageUrl"]}" alt=""></th>`
+            catagory.innerHTML += `<td scope="col">${dataElems[i]["metadata"]["category"]["displayValue"]}</td>`
             kills.innerHTML += `<td scope="col">${dataElems[i]["stats"]["kills"]["displayValue"]}</td>`
             shotsFired.innerHTML += `<td scope="col">${dataElems[i]["stats"]["shotsFired"]["displayValue"]}</td>`
             shotsHit.innerHTML += `<td scope="col">${dataElems[i]["stats"]["shotsHit"]["displayValue"]}</td>`
@@ -68,6 +72,31 @@ function stat(name){
 
     fetch(
         `https://public-api.tracker.gg/v2/csgo/standard/search?TRN-Api-Key=de89f9b3-26ff-433d-bcec-e3bbb477dfdd&platform=steam&query=${name}`, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0',
+                Accept: 'application/json',
+                'Accept-Encoding': 'gzip',
+                'TRN-Api-Key': 'de89f9b3-26ff-433d-bcec-e3bbb477dfdd',
+            },
+            mode:'cors',
+        }
+
+    )
+    .then(response => response.json())
+        .then(function(data){
+            profileDiv.innerHTML = `<img class="profile mr-3 float-left" src="${data["data"][0]["avatarUrl"]}" alt="">
+        <h2>${name.toUpperCase()}</h2>`;
+        }
+        )
+        .catch(error => {
+            console.log(error);
+    });
+}
+
+function detail(name){
+
+    fetch(
+        `https://public-api.tracker.gg/v2/csgo/standard/profile/steam/${name}/?TRN-Api-Key=de89f9b3-26ff-433d-bcec-e3bbb477dfdd`, {
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0',
                 Accept: 'application/json',
