4847181926059a5ad5e1268c2ddab5751473f4d4
keiredin <se.keiredin.aman@gmail.com>
2021-03-03 22:54:54 +0300

    function to fetch data from the api added

--- assets/js/index1.js
+++ assets/js/index1.js
@@ -29,3 +29,28 @@ ps.addEventListener('click',()=>{
 origin.addEventListener('click',()=>{
     collector(name, "origin")
 });
+
+
+function search(name, platform){
+    fetch(
+        `https://public-api.tracker.gg/v2/apex/standard/profile/${platform}/${name}?TRN-Api-Key=87d61054-b4ab-4722-b99b-35a2e7ee0392`, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0',
+                Accept: 'application/json',
+                'Accept-Encoding': 'gzip',
+                'TRN-Api-Key': 'de89f9b3-26ff-433d-bcec-e3bbb477dfdd',
+            },
+            mode:'cors',
+        }
+
+    )
+    .then(response => response.json())
+    .then(function(data){
+        if (Object.keys(data)[0] == 'errors'){
+            let i;
+            profileDiv.innerHTML = `<img class="profile mr-3 float-left" src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png" style = "background-color: white;" width="12%" height="100%" alt="">
+        <h2>Player Not Found</h2>`;
+        overview.innerHTML = ''
+        legendDisp.innerHTML = ""
+            return;
+        }
